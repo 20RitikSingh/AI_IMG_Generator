a69adb57085f7adb97c9e84dca5eaef9958031b2
ritik <20ritiksingh@gmail.com>
2023-05-30 13:39:17 +0530

    first commit

---
+++ server/mongodb/connect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = (url)=>{
+    mongoose.set('strictQuery',true);
+    mongoose.connect(url)
+        .then(()=>console.log('MongoDB connected'))
+        .catch((err)=>{
+            console.error('failed to connect with mongo');
+            console.log(err);
+        })
+
+};  
+
+export default connectDB;
